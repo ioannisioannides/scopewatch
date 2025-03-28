469455739d598ed2d2bb3bf4646da4af264f9579
Ioannis Ioannides <i.ioannides@gmail.com>
2025-03-28 13:06:57 +0200

    Update views.py

---
+++ apps/consultants/views.py
@@ -0,0 +1,15 @@
+# apps/consultants/views.py
+
+from django.http import HttpResponse
+
+def consultant_list_view(request):
+    """
+    Placeholder view for listing consultants or returning JSON.
+    """
+    return HttpResponse("Consultant List Placeholder")
+
+def consultancy_firm_list_view(request):
+    """
+    Placeholder view for listing consultancy firms.
+    """
+    return HttpResponse("Consultancy Firm List Placeholder")
