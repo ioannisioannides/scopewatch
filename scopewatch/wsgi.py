59d98353db49d47037310fafeeeca1d567949e35
Ioannis Ioannides <i.ioannides@gmail.com>
2025-03-28 11:50:08 +0200

    Update wsgi.py

--- scopewatch/wsgi.py
+++ scopewatch/wsgi.py
@@ -1,1 +1,12 @@
+"""
+WSGI config for the Scopewatch project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scopewatch.settings')
+
+application = get_wsgi_application()
