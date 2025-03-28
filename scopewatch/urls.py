790e83ec6d4759d9fd74d3c8d45b2509e7992cd2
Ioannis Ioannides <i.ioannides@gmail.com>
2025-03-28 11:50:37 +0200

    Update urls.py

--- scopewatch/urls.py
+++ scopewatch/urls.py
@@ -1,1 +1,10 @@
+"""
+URL configuration for the Scopewatch project.
+"""
 
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
