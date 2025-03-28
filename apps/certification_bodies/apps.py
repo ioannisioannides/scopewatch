0ce27aedd3cfc433e7737029d541a98db52c5110
Ioannis Ioannides <i.ioannides@gmail.com>
2025-03-28 12:50:42 +0200

    Update apps.py

---
+++ apps/certification_bodies/apps.py
@@ -0,0 +1,8 @@
+# apps/certification_bodies/apps.py
+
+from django.apps import AppConfig
+
+class CertificationBodiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.certification_bodies'
+    verbose_name = 'Certification Bodies'
