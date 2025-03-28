af1d1f7d20b137caffc5165bb03c5965baf0b268
Ioannis Ioannides <i.ioannides@gmail.com>
2025-03-28 12:20:13 +0200

    Update apps.py

--- apps/audits/apps.py
+++ apps/audits/apps.py
@@ -1,1 +1,8 @@
+# apps/audits/apps.py
 
+from django.apps import AppConfig
+
+class AuditsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.audits'
+    verbose_name = 'Audits'
