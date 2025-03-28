29c99ad777122409340559e4febc592d88b63aeb
Ioannis Ioannides <i.ioannides@gmail.com>
2025-03-28 13:05:57 +0200

    Update apps.py

---
+++ apps/consultants/apps.py
@@ -0,0 +1,8 @@
+# apps/consultants/apps.py
+
+from django.apps import AppConfig
+
+class ConsultantsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.consultants'
+    verbose_name = 'Consultants'
