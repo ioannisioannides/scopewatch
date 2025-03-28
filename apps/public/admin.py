409d91306c9021d2c08913ea0f53a6d7607f8f8b
Ioannis Ioannides <i.ioannides@gmail.com>
2025-03-28 13:08:36 +0200

    Update admin.py

---
+++ apps/public/admin.py
@@ -0,0 +1,9 @@
+# apps/public/admin.py
+
+from django.contrib import admin
+
+# Typically, you might not have admin models for "public" unless you store
+# specific data in this app's models.py
+# Example:
+# from .models import PublicLog
+# admin.site.register(PublicLog)
