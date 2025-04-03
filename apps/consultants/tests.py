87cd15d0be98aa3db16fb36659369be7893e21e2
Ioannis Ioannides <i.ioannides@gmail.com>
2025-04-03 03:17:34 +0300

    📝 Refactor Consultant and ConsultancyFirm models; improve string representation and add view templates for listing consultants and firms

--- apps/consultants/tests.py
+++ apps/consultants/tests.py
@@ -7,27 +7,20 @@ This module contains test cases for the Consultant and ConsultancyFirm models.
 These tests ensure that the models behave as expected when creating and validating instances.
 """
 
-from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
-
-from .models import ConsultancyFirm, Consultant
+from django.contrib.auth.models import User
+from .models import Consultant, ConsultancyFirm
 
 
 class ConsultantsModelTest(TestCase):
     """
     Test suite for the Consultant and ConsultancyFirm models.
-
-    This class contains unit tests for creating and validating instances
-    of the Consultant and ConsultancyFirm models.
     """
 
     def test_create_consultant(self):
         """
         Test the creation of a Consultant instance.
-
-        This test ensures that a Consultant instance can be created with valid data
-        and that its attributes are correctly set.
         """
         user = User.objects.create_user(username="consultant_user")
         consultant = Consultant.objects.create(user=user, specialty="ISO 9001", is_active=True)
@@ -38,9 +31,6 @@ class ConsultantsModelTest(TestCase):
     def test_create_consultancy_firm(self):
         """
         Test the creation of a ConsultancyFirm instance.
-
-        This test ensures that a ConsultancyFirm instance can be created with valid data
-        and that its attributes are correctly set.
         """
         firm = ConsultancyFirm.objects.create(name="Global Consulting")
         self.assertEqual(firm.name, "Global Consulting")
@@ -48,17 +38,30 @@ class ConsultantsModelTest(TestCase):
 
 
 class ConsultantViewTest(TestCase):
+    """
+    Test suite for the Consultant views.
+    """
+
     def setUp(self):
+        """
+        Set up test data for the Consultant views.
+        """
         self.user = User.objects.create_user(username="consultant_user")
         self.consultant = Consultant.objects.create(user=self.user, specialty="ISO 9001")
         self.firm = ConsultancyFirm.objects.create(name="Test Firm")
 
     def test_consultant_list_view(self):
+        """
+        Test the consultant list view.
+        """
         response = self.client.get(reverse("consultant_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "ISO 9001")
 
     def test_consultancy_firm_list_view(self):
+        """
+        Test the consultancy firm list view.
+        """
         response = self.client.get(reverse("consultancy_firm_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Firm")
